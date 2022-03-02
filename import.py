e97b45ba0cfd46f60f227a8618ca66731399815c
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2022-03-02 14:32:49 +0300

    add import in line

---
+++ import.py
@@ -0,0 +1,11 @@
+
+
+def import_inline():
+    with open('test.txt', 'r', encoding='utf-8') as data:
+            with open('C:\Pratice_code\phonebook\generation_data copy.txt', 'a', encoding='utf-8') as file:
+                file.write('\n')
+                for line in data:
+                    file.write(line)
+            data.close()
+
+
