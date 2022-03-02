6194188f7eb7c918df56f0012384febeb8ed2b3a
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2022-03-02 15:46:32 +0300

    unfinished

--- import.py
+++ import.py
@@ -1,5 +1,7 @@
 
 
+
+
 def import_inline():
     with open('test.txt', 'r', encoding='utf-8') as data:
             with open('C:\Pratice_code\phonebook\generation_data copy.txt', 'a', encoding='utf-8') as file:
@@ -8,4 +10,4 @@ def import_inline():
                     file.write(line)
             data.close()
 
-
+import_inline()
